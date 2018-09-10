c10ced1440f15749c1d44c3874273fa8ddc76638
Marcus <marcus.kerr94@gmail.com>
2018-09-10 12:11:09 +0100

    Feature: making a deposit, UT - RED

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,5 +1,22 @@
 require 'BankAccount'
-
 describe BankAccount do
+  let(:bank_account) { subject }
+
+  describe 'initialize' do
+    it 'initializes with an 0 balance' do
+      expect(bank_account.balance).to eq 0
+    end
+  end
   
+  describe '#deposit' do
+    it 'increases balance by deposited amount' do
+      expect { bank_account.deposit(50) }.to change(bank_account.balance). by 50 
+    end 
+  end 
+
+  describe '#balance' do
+    it 'returns the balance' do
+      expect(bank_account.balance).to eq 0
+    end 
+  end 
 end
